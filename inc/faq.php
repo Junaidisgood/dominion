57eeaa241a4d78247385ea38bddd1bfd635a423f
Junaid Bashir <junaid@crayondevelopers.com>
2021-03-21 04:34:52 +0400

    faq rework

--- inc/faq.php
+++ inc/faq.php
@@ -1,23 +1,44 @@
 <div id="faq">
-	<h1>F.A.Q.</h1>
-	<details>
-		<summary>Question 1</summary>
-		<p>Just use a <code>details</code> and <code>summary</code> tags with a little CSS, no more JS. You can use any text or emoji as caret.</p> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora in nihil, deleniti saepe optio corrupti dolorum perferendis hic iure, maiores quae vitae provident minus, ipsum, aut tempore facilis doloremque rem.</p>
-	</details>
-	<details>
-		<summary>Question 2</summary>
-		<p>Ipsa veritatis inventore reprehenderit dolorem, officiis. Quaerat doloribus voluptatibus impedit repellat quae perspiciatis nesciunt, recusandae facilis unde fugit et eaque rem voluptatum perferendis libero veniam rerum aliquam eos minima voluptate.</p>
-	</details>
-	<details>
-		<summary>Question 3</summary>
-		<p>Quisquam, necessitatibus quo dolore sequi suscipit magni, voluptatum debitis, accusantium dolorum officia beatae rerum similique optio saepe vel dicta facere modi, voluptatem culpa expedita quaerat eum reprehenderit dolores. Assumenda, doloremque!</p>
-	</details>
-	<details>
-		<summary>Question 4</summary>
-		<p>Dolorum quaerat facilis magnam commodi molestiae atque similique hic enim pariatur nulla magni amet iusto soluta nemo alias odit recusandae repudiandae dignissimos, fugit asperiores quia eius necessitatibus iure. Possimus, illum.</p>
-	</details>
-	<details>
-		<summary>Question 5</summary>
-		<p>Consequuntur voluptate consequatur sed adipisci libero temporibus atque itaque voluptas eos, rerum vero qui. Adipisci illum, molestias commodi unde necessitatibus ea, quod explicabo, deleniti voluptatem aliquam reprehenderit, nobis maiores. Saepe?</p>
-	</details>
+	<center><h1>Click on them to see the Answers</h1></center>
+
+	<div class="faqs-container" itemscope itemtype="https://schema.org/FAQPage">
+		
+		<div class="faq-singular" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+			<h2 class="faq-question" itemprop="name">What is the return policy?</h2>
+			<div class="faq-answer" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+				<div itemprop="text">
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+				</div>
+			</div>
+		</div>
+		
+		<div class="faq-singular" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+			<h2 class="faq-question" itemprop="name">How long does it take to process a refund?</h2>
+			<div class="faq-answer" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+				<div itemprop="text">
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+				</div>
+			</div>
+		</div>
+		
+		<div class="faq-singular" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+			<h2 class="faq-question" itemprop="name">What is the policy for late/non-delivery of items ordered online?</h2>
+			<div class="faq-answer" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+				<div itemprop="text">
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+				</div>
+			</div>
+		</div>
+		
+		<div class="faq-singular" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+			<h2 class="faq-question" itemprop="name">Is the product same as shown in pictures?</h2>
+			<div class="faq-answer" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+				<div itemprop="text">
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+				</div>
+			</div>
+		</div>
+		
+	</div>
+
 </div>
