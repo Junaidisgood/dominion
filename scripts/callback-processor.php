aee959dd1606c5d7fb9f43deb7c776f473f55f31
Junaid Bashir <junaid@crayondevelopers.com>
2021-03-22 20:46:29 +0400

    update email receiver

--- scripts/callback-processor.php
+++ scripts/callback-processor.php
@@ -5,7 +5,8 @@ $phone = $_POST['phone'];
 $body = "A callback was requested by " . $name . "\nDetails:\nEmail: " . $email . "\Phone: " . $phone;
 if(isset($_POST['callback-btn'])) {
 
-mail("jb49195@gmail.com", "DFC - New Callback Request",
+mail(
+        "dominionco@gmail.com", "DFC - New Callback Request",
 
 $body);
 
