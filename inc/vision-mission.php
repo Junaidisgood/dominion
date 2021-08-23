e25f88650140303e88e71a1c12368bcc6e2c1adc
Junaid Bashir <junaid@crayondevelopers.com>
2021-08-23 12:27:13 +0100

    fix mission/vision section

--- inc/vision-mission.php
+++ inc/vision-mission.php
@@ -10,54 +10,27 @@
                     </div>
                     <div class="col-lg-6">
                         <div class="vission-content-wrap">
-                            <div id="tab_1" class="vission-tab-content">
+                            <div id="tab_1" class="vission-tab-content mt-5">
                                 <div class="section-title">
                                     <h2>Mission</h2>
                                 </div>
                                 <p>To build wealth, manage assets and create liquid capital for businesses, governments and individuals globally and to be a global resource for project funding, infrastructure development and other liquidity assets.</p>
                                 <p>Mobilize the business world to bring development solutions to emerging economies.</p>
-                                <div class="success-item bg-snow">
-                                    <div class="icon-image">
-                                        <img src="assets/images/services/icon2.png" alt="Success Icon Image">
-                                    </div>
-                                    <div class="success-content">
-                                        <span class="count-text" data-speed="2500" data-stop="568">0</span>
-                                        <h5>Complete Projects.</h5>
-                                    </div>
-                                </div>
                             </div>
-                            <div id="tab_2" class="vission-tab-content active">
+                            <div id="tab_2" class="vission-tab-content active mt-5">
                                 <div class="section-title">
                                     <h2>Vision</h2>
                                 </div>
                                 <p>To innovate, create, promote and build humanitarian and economic projects globally.</p>
                                 <p>To be distinguished as a respected multinational finance related administrations pioneer, trusted accomplice, and supplier of imaginative answers for developing wealth. Contribute to instill balance into globalization.</p>
-                                <div class="success-item bg-snow">
-                                    <div class="icon-image">
-                                        <img src="assets/images/about/icon.png" alt="Success Icon Image">
-                                    </div>
-                                    <div class="success-content">
-                                        <span class="count-text" data-speed="2500" data-stop="890">0</span>
-                                        <h5>Happy Customers.</h5>
-                                    </div>
-                                </div>
                             </div>
-                            <div id="tab_3" class="vission-tab-content">
+                            <div id="tab_3" class="vission-tab-content mt-5">
                                 <div class="section-title">
                                     <h2>Values</h2>
                                 </div>
                                 <p>From our founding, we have held to the conviction that there is a different way for an investment firm to create value. Our commitment to create lasting impact means that we work differently, tenacious in our desire to unearth the fundamentals that drive businesses and markets over the long-term. And it means that we partner differently, aligning our interests with those of our investors for lasting impact.</p>
                                 <p>Living our values, we create lasting impact for our investors, teams, businesses and the communities in which we live.
 								</p>
-                                <div class="success-item bg-snow">
-                                    <div class="icon-image">
-                                        <img src="assets/images/services/icon6.png" alt="Success Icon Image">
-                                    </div>
-                                    <div class="success-content">
-                                        <span class="count-text" data-speed="2500" data-stop="134">0</span>
-                                        <h5>Winning Awards.</h5>
-                                    </div>
-                                </div>
                             </div>
                         </div>
                     </div>
